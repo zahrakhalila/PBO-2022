50918616dea1120b18f289efe67b6d508455a76f
Zahra Khalila Salsabila <89764392+zahrakhalila@users.noreply.github.com>
2022-08-28 21:35:08 +0700

    Add files via upload

---
+++ MyProject2/src/myproject2/MyPBO012.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproject2;
+
+/**
+ *
+ * @author user
+ */
+public class MyPBO012 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("ZZZZZZZZ\t AA\t    HH\t     HH");
+        System.out.println("     ZZ\t\tAAAA\t    HH\t     HH");
+        System.out.println("    ZZ\t       AA  AA\t    HH\t     HH");
+        System.out.println("   ZZ\t      AA    AA      HHHHHHHHHHH");
+        System.out.println("  ZZ\t     AAAAAAAAAA     HH\t     HH");
+        System.out.println(" ZZ\t    AA        AA    HH\t     HH");
+        System.out.println("ZZZZZZZZ   AA          AA   HH\t     HH");
+    }
+    
+}
