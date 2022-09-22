299415244caafe10271157f50ecbb4a004baa7ad
Zahra Khalila Salsabila <89764392+zahrakhalila@users.noreply.github.com>
2022-09-22 12:09:23 +0700

    Add files via upload

---
+++ ProjectOverloading1/src/projectoverloading1/MyClass.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectoverloading1;
+
+/**
+ *
+ * @author user
+ */
+public class MyClass {
+    void myMethod(){
+        System.out.println("Hello");
+    }
+    
+    void myMethod(int x){
+        System.out.println("Holla");
+    }
+    
+    void myMethod(int x, int y){
+        System.out.println("Hollo");
+    }
+    
+    void myMethod(double x){
+        System.out.println("Hilli");
+    }
+    
+    void myMethod(int x, double y){
+        System.out.println("Hullo");
+    }
+    
+    String myMethod(String x){
+        return "Hello";
+    }
+    
+    void myMethod(int x, String y){
+        System.out.println("Holli");
+    }
+}
---
+++ ProjectOverloading1/src/projectoverloading1/ProjectOverloading1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectoverloading1;
+
+/**
+ *
+ * @author user
+ */
+public class ProjectOverloading1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MyClass m = new MyClass();
+
+            m.myMethod();
+            m.myMethod(1.20);
+            m.myMethod(2, 2.0);
+    }
+    
+}
